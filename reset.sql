524f1204333023e2f4687fa296e447594e14df90
BradleyRobertTaylor <taylorbradleyr@gmail.com>
2023-10-19 08:04:50 -0700

    Update flag schema with updated at and flag key

--- reset.sql
+++ reset.sql
@@ -3,10 +3,10 @@ DROP TABLE IF EXISTS flags;
 CREATE TABLE
   flags (
     id serial PRIMARY KEY,
+    f_key varchar(20) NOT NULL UNIQUE,
     title varchar(100) NOT NULL UNIQUE,
     description text,
     is_active boolean DEFAULT false NOT NULL,
-    created_at timestamp
-    with
-      time zone NOT NULL DEFAULT current_timestamp
+    created_at timestamp with time zone NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp with time zone NOT NULL DEFAULT current_timestamp
   );
---
+++ seed-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+  flags (f_key, title, description)
+VALUES
+  ('flag-1', 'Flag 1', 'My cool first flag.'),
+  ('flag-2', 'Flag 2', 'My second flag.')
