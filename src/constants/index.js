45ff0e021d5d8e9caa99a304b0dc0632b41e52f0
isaacylee <isaaclee@Isaacs-iMac.local>
2023-11-01 14:41:50 -0700

    Added NATS Jetstream Reply for SDK Key Validation

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,4 +12,17 @@ export const FLAG_STREAM_CONSUMER_CONFIG = {
   ack_policy: AckPolicy.Explicit,
 };
 
+export const SDK_KEY_STREAM_CONFIG = {
+  name: "SDK_KEY",
+  subjects: ["SDK_KEY.*"],
+  storage: "memory",
+  max_msgs: 1,
+};
+
+export const SDK_KEY_STREAM_CONSUMER_CONFIG = {
+  durable_name: "SdkKeyStream",
+  ack_policy: AckPolicy.Explicit,
+};
+
 export const KEY_BITE_LEN = 20;
+export const IV_BYTE_LEN = 16;
