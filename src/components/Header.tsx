d060752890c114c936a87af3581d95d4e9e9268f
Bradley Taylor <taylorbradleyr@gmail.com>
2023-11-06 17:30:13 -0800

    Disable search bar on sdk key page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,37 +22,31 @@ function Header({ setSidebarOpen }: HeaderProps) {
         <Bars3Icon className="h-6 w-6" aria-hidden="true" />
       </button>
 
-      {!match && (
-        <>
-          {/* Separator */}
-          <div
-            className="h-6 w-px bg-gray-900/10 lg:hidden"
+      {/* Separator */}
+      <div className="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true" />
+      <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
+        <form className="relative flex flex-1" action="#" method="GET">
+          <label htmlFor="search-field" className="sr-only">
+            Search
+          </label>
+          <MagnifyingGlassIcon
+            className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
             aria-hidden="true"
           />
-          <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-            <form className="relative flex flex-1" action="#" method="GET">
-              <label htmlFor="search-field" className="sr-only">
-                Search
-              </label>
-              <MagnifyingGlassIcon
-                className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
-                aria-hidden="true"
-              />
-              <input
-                id="search-field"
-                className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setSearch(e.target.value)
-                }
-                value={search}
-                placeholder="Search..."
-                type="search"
-                name="search"
-              />
-            </form>
-          </div>
-        </>
-      )}
+          <input
+            disabled={!!match}
+            id="search-field"
+            className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setSearch(e.target.value)
+            }
+            value={search}
+            placeholder="Search..."
+            type="search"
+            name="search"
+          />
+        </form>
+      </div>
     </div>
   );
 }
