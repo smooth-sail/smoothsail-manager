36bb0a11a1a82d3876fa1080dec81ee8379402e0
BradleyRobertTaylor <taylorbradleyr@gmail.com>
2023-11-07 20:35:54 -0800

    Statically serve dist folder of ui from express server. Proxy api requests to proper endpoints during development.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,12 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000",
+      "/key": "http://localhost:3000",
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
